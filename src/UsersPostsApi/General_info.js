1d8934e0bb77a063b9af537079fb49548067c95b
drawlin <itshoussameddine@gmail.com>
2024-11-29 00:57:10 +0100

    Version 1

--- src/UsersPostsApi/General_info.js
+++ src/UsersPostsApi/General_info.js
@@ -1,6 +1,11 @@
 import React, {useState} from 'react';
+import Post_Info from './Post_info';
 import './style.css'
 function General_info(props){
+
+    const [postState, setPostState] = useState(false);
+
+
     return(
         <div>
             {props.data.map((element) => {
@@ -9,7 +14,8 @@ function General_info(props){
                         <p>Nom: {element.name}</p>
                         <p>Email: {element.email}</p>
                         <p>Ville: {element.address.city}</p>
-                        <button>detail posts</button>
+                        <button onClick={() => setPostState(ps => !ps)}>detail posts</button>
+                        {postState && <Post_Info userId={element.id}/>}
                     </div>
                 )
             })}
---
+++ src/UsersPostsApi/Post_info.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+
+
+function Post_Info(props){
+    
+    const [data, setData] = useState([]);
+    const [numOfPosts, setNumOfPosts] = useState(0);
+
+
+    useEffect(()=>{
+
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(response => {
+            const filteredData = response.filter(element => element.userId === props.userId);
+            setData(filteredData);
+            setNumOfPosts(filteredData.length)
+        }    
+                    
+        )
+    }, []);
+
+    return(
+        <div>
+            <h3>Nombres des posts: {numOfPosts}</h3>
+            {data.map(element => {
+                return(
+                    <div className='posts'>
+                        <h3>{element.title}</h3>
+                        <p>{element.body}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Post_Info;

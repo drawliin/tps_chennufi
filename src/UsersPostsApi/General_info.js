6e39c096972467c8f76297285f5fc7978a2fa9a0
drawlin <itshoussameddine@gmail.com>
2024-11-29 23:27:01 +0100

    Added Toggle Function

--- src/UsersPostsApi/General_info.js
+++ src/UsersPostsApi/General_info.js
@@ -3,18 +3,15 @@ import Post_Info from './Post_info';
 
 function General_info(props){
 
-    const [postState, setPostState] = useState(false);
-    const [activeId, setActiveId] = useState(null);
+    const [activeId, setActiveId] = useState({});
 
     
 
     function handlePostPreview(id) {
-    if (activeId === id) {
-        setPostState(!postState);
-    } else {
-        setActiveId(id);
-        setPostState(true);
-    }
+        setActiveId((prev) => ({
+            ...prev,
+            [id]: !prev[id]
+        }));
 }
 
     
@@ -31,7 +28,7 @@ function General_info(props){
                         <p><b>Email</b>: {element.email}</p>
                         <p><b>Ville</b>: {element.address.city}</p>
                         <button onClick={() => handlePostPreview(element.id)}>detail posts</button>
-                        {postState && activeId === element.id && <Post_Info userId={element.id} />}
+                        {activeId[element.id] && <Post_Info userId={element.id} />}
                     </div>
                 )
             })}
